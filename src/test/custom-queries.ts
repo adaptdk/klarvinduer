75fe750f00906db0adadf35d7bb5fd41b7dac79f
Casper Engelmann <me@casperengelmann.com>
2021-04-20 16:11:55 +0200

    style: explicit return

--- src/test/custom-queries.ts
+++ src/test/custom-queries.ts
@@ -1,12 +1,16 @@
 import { queryHelpers, buildQueries } from '@testing-library/react'
 
-const queryAllByDataTestid = (c: HTMLElement, dataTestidValue: any) =>
-  queryHelpers.queryAllByAttribute('data-testid', c, dataTestidValue)
+const queryAllByDataTestid = (c: HTMLElement, dataTestidValue: any) => {
+  return queryHelpers.queryAllByAttribute('data-testid', c, dataTestidValue)
+}
 
-const getMultipleError = (_: HTMLElement, dataTestidValue: any) =>
-  `Found multiple elements with the data-cy attribute of: ${dataTestidValue}`
-const getMissingError = (_: HTMLElement, dataTestidValue: any) =>
-  `Unable to find an element with the data-cy attribute of: ${dataTestidValue}`
+const getMultipleError = (_: HTMLElement, dataTestidValue: any) => {
+  return `Found multiple elements with the data-cy attribute of: ${dataTestidValue}`
+}
+
+const getMissingError = (_: HTMLElement, dataTestidValue: any) => {
+  return `Unable to find an element with the data-cy attribute of: ${dataTestidValue}`
+}
 
 const [
   queryByDataTestid,

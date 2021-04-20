f9190bbbb8f75137af119624e102c07fac616e4a
Casper Engelmann <me@casperengelmann.com>
2021-04-20 11:09:21 +0200

    moved + added custom queries + file mocks

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,9 +1,10 @@
-import React, { FC, ReactElement } from 'react'
+import { FC, ReactElement } from 'react'
 import { queries, render, RenderOptions } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
 import * as customQueries from './custom-queries'
 
-const AllTheProviders: FC = ({ children }) => {
+const Providers: FC = ({ children }) => {
   return <>{children}</>
 }
 
@@ -13,7 +14,7 @@ const customRender = (
 ) =>
   render(ui, {
     queries: { ...queries, ...customQueries },
-    wrapper: AllTheProviders,
+    wrapper: Providers,
     ...options,
   })
 
